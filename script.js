ed0a9676c9bc199abca20d223313ec2484653752
Mookgol <138899392+Mookgol@users.noreply.github.com>
2023-09-19 13:10:21 +0200

    styled the service cards

--- script.js
+++ script.js
@@ -1,33 +1,6 @@
 //slideshow code//
-const cards = document.querySelectorAll('.card');
-const paginationButtons = document.querySelectorAll('.pagination button');
-let currentIndex = 0;
 
-function showCard(index) {
-  cards.forEach((card, i) => {
-    if (i === index) {
-      card.style.display = 'flex';
-    } else {
-      card.style.display = 'none';
-    }
-  });
-}
-
-function nextCard() {
-  currentIndex = (currentIndex + 1) % cards.length;
-  showCard(currentIndex);
-}
-
-function previousCard() {
-  currentIndex = (currentIndex - 1 + cards.length) % cards.length;
-  showCard(currentIndex);
-}
-
-// Show the initial card
-showCard(currentIndex);
-
-// Auto-advance to the next card every 5 seconds (adjust as needed)
-setInterval(nextCard, 5000); // Change the interval duration (in milliseconds) as needed
+ 
 
 
     document.addEventListener("DOMContentLoaded", function () {

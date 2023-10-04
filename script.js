004d0cd8b020f52c8713f0d91b44aaef67a209fb
Likhaya <99352117+likeLikhaya@users.noreply.github.com>
2023-10-04 10:44:27 +0200

    Fixing the errors on js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 
     
 //slideshow code//
-
+document.addEventListener("DOMContentLoaded", function () {
 let slideIndex = 0; 
 showSlides();
-
 
 function plusSlides(n) {
     showSlides((slideIndex += n));
 }
-
-
 function currentSlide(n) {
     showSlides((slideIndex = n - 1)); 
 }
@@ -20,30 +17,25 @@ function showSlides() {
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
 
-    
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
-    
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
 
-    
     slideIndex++;
     if (slideIndex > slides.length) {
         slideIndex = 1; 
     }
 
-    
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
 
-
 setInterval(showSlides, 3000);
-
+});
 
 
     document.addEventListener("DOMContentLoaded", function () {
@@ -90,6 +82,7 @@ setInterval(showSlides, 3000);
 
    // script.js
 
+   document.addEventListener("DOMContentLoaded", function () {
    function updateCurrentTime() {
     const currentTimeElement = document.getElementById("current-time");
 
@@ -100,7 +93,7 @@ setInterval(showSlides, 3000);
 
     const currentTimeString = `${hours}:${minutes}:${seconds}`;
     currentTimeElement.textContent = `CurrentTime: ${currentTimeString}`;
-}
+
 
 // Update the current time every second
 setInterval(updateCurrentTime, 1000);
@@ -113,16 +106,17 @@ $(document).ready(function() {
       $('.menu-links').toggleClass('show-menu');
   });
 });
+}});
+
 
 //Making an image bounce
+document.addEventListener("DOMContentLoaded", function () {
 const image = document.querySelector('.bounce-image');
 
 function toggleBounce() {
     image.classList.toggle('bounce');
 }
-
 const navbar = document.querySelector('.navbar');
-
 window.addEventListener('scroll', () => {
   if (window.scrollY > 50) {
     navbar.classList.add('fixed');
@@ -130,10 +124,72 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('fixed');
   }
 });
+});
 
+// Array Buffers
+  // Collect form data
+  document.addEventListener("DOMContentLoaded", function () {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const number = document.getElementById('number').value;
+  const message = document.getElementById('message').value;
 
+  // Convert data to an ArrayBuffer
+  const encoder = new TextEncoder();
+  const nameArrayBuffer = encoder.encode(name).buffer;
+  const emailArrayBuffer = encoder.encode(email).buffer;
+  const numArrayBuffer = encoder.encode(number).buffer;
+  const messageArrayBuffer = encoder.encode(message).buffer;
 
+  // Log the ArrayBuffers to the console
+  console.log(nameArrayBuffer);
+  console.log(emailArrayBuffer);
+  console.log(messageArrayBuffer);
+  });
 
+// Form Data
+document.addEventListener("DOMContentLoaded", function () {
+  formElem.onsubmit = async (e) => {
+    e.preventDefault();
 
+    let respone = await fetch('/article/formdata/post/user',{
+        method: 'POST',
+        body: new FormData (formElem)
 
+    });
+
+    let result = await respone.json();
+
+    alert(result.message);
+}
+});
  
+// not sure
+document.addEventListener("DOMContentLoaded", function () {
+window.addEventListener('scroll', function(){
+    var navbar= this.document.querySelector("navbar");
+    navbar.classList.toggle("sticky", window.scrollY > 0);
+});
+});
+
+// not really which code is this
+document.addEventListener("DOMContentLoaded", function () {
+window.onload = () => {
+    document.getElementById('submitbtn').addEventListener("submit");
+    }});
+    
+// back to top btn 
+document.addEventListener("DOMContentLoaded", function () {
+
+const images = document.querySelectorAll('.about-us-images');
+
+images.forEach(image => {
+    image.addEventListener('mouseenter', () => {
+        image.classList.add('image-hover'); // Add the hover class
+    });
+
+    image.addEventListener('mouseleave', () => {
+        image.classList.remove('image-hover'); // Remove the hover class
+    });
+});
+});

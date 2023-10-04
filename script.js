cec4d230f973baf10f5e2f14768ac4c43f159842
Mookgol <138899392+Mookgol@users.noreply.github.com>
2023-10-04 11:16:00 +0200

    changes

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ setInterval(showSlides, 3000);
                 alert("Please enter your name.");
             }
     
-            // Email validation
+            // Email validations
             const emailInput = document.getElementById("email");
             const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
             if (!emailPattern.test(emailInput.value)) {
